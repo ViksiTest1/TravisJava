79ad08bb3e74097ec69589fe147f890115b49433
ViksiTest1 <rade.knezevic.bd+1@gmail.com>
2024-11-17 19:23:24 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class SecretPrinter {
+
+    public static void main(String[] args) {
+        // Printing the provided statements
+        print("My token: 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");
+        print("My AWS_CLIENT_SECRET=[abcdefghijklamneops]");
+        print("My AWS_CLIENT_SECRET=[AKIA3232534534233233]");
+        print("My AWS_CLIENT_SECRET=wJalrXUtnFEMI/K7MDENG/bPxRfiCY/shshshsh");
+        print("Is this a key? = MIIG5AIBAAKCAYEAzjrEWNDLExn...==");
+    }
+
+    // Method to print statements
+    public static void print(String statement) {
+        System.out.println(statement);
+    }
+}
